545f649837963940fde5b00ec800eb6cc7a5dc59
maxrabets <rabetsmaxim@gmail.com>
2021-03-03 23:21:17 +0300

    Добавьте файлы проекта.

---
+++ lab1_2010/MainForm.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1_2010
+{
+    public partial class MainForm : Form
+    {
+        private bool[,] bitArray;
+        private int[,] bitArray2;
+        private int[,] bitArray3;
+        Bitmap bitmap;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private bool findRadius(int i, int j, int i1, int j1, int R)
+        {
+            if (j1 < 0 || i1 < 0 || i1 >= bitmap.Height || j1 >= bitmap.Width) return false;
+            if (bitArray[i, j] != bitArray[i1, j1])
+            {
+                //ПРОТИВОПОЛОЖНЫЙ ПИКСЕЛЬ НАЙДЕН 
+                if (bitArray[i, j] == false)
+                    bitArray2[i, j] = -R;
+                else
+                    bitArray2[i, j] = R;
+                return true;
+            }
+            return false;
+        }
+
+        private int findMax(int[,] bitArray)
+        {
+            int width = bitArray.GetLength(0);
+            int height = bitArray.GetLength(1);
+            int max = bitArray[0, 0];
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (bitArray[i, j] > max)
+                        max = bitArray[i, j];
+                }
+            }
+            return max;
+        }
+
+        private int findMin(int[,] bitArray)
+        {
+            int width = bitArray.GetLength(0);
+            int height = bitArray.GetLength(1);
+            int min = bitArray[0, 0];
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (bitArray[i, j] < min)
+                        min = bitArray[i, j];
+                }
+            }
+            return min;
+        }
+
+        private int[,] convertBinToGray(int[,] bitArray1)
+        {
+            int width = bitArray1.GetLength(0);
+            int height = bitArray1.GetLength(1);
+            int[,] bitArray2 = new int[width, height];
+            int min = findMin(bitArray1);
+            int max = findMax(bitArray1);
+            int interval = 255 / (max - min);
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    bitArray2[i, j] = 255 - interval*(bitArray1[i,j] - min);
+                }
+            }
+            return bitArray2;
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            string fileName;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+                //Image image = Bitmap.FromFile(fileName);
+                bitmap = (Bitmap)Bitmap.FromFile(fileName);
+                Bitmap newBitmap = new Bitmap(bitmap.Width, bitmap.Height, bitmap.PixelFormat);
+                pictureBox1.Image = bitmap;
+                pictureBox2.Image = newBitmap;
+                bitArray = new bool[bitmap.Width, bitmap.Height];
+                bitArray2 = new int[bitmap.Width, bitmap.Height];
+                dataGridView1.Rows.Clear();
+                dataGridView2.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                dataGridView2.Columns.Clear();
+                for (int i = 0; i < bitmap.Height; i++)
+                {
+                    dataGridView1.Columns.Add(i.ToString(), i.ToString());
+                    dataGridView2.Columns.Add(i.ToString(), i.ToString());
+                    dataGridView1.Columns[i].Width = 20;
+                    dataGridView2.Columns[i].Width = 20;
+                }
+                dataGridView1.Rows.Add(bitmap.Height);
+                dataGridView2.Rows.Add(bitmap.Height);
+                
+                for (int i = 0; i < bitmap.Width; i++)
+                {
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        Color pixel = bitmap.GetPixel(i, j);
+                        bitArray[i, j] = pixel.R == 0 && pixel.G == 0 && pixel.B == 0;
+                        if(pixel.R == 0)
+                            dataGridView1.Rows[j].Cells[i].Value = 1;
+                        else
+                            dataGridView1.Rows[j].Cells[i].Value = 0;
+                        //bitmapTextBox.Text += "(" + pixel.A + " " + pixel.R + " " + pixel.G + " " + pixel.B + ") "; //bitArray[i, j].ToString();
+                    }
+                }
+
+                Bitmap bitmap2 = new Bitmap(bitmap.Width, bitmap.Height);
+                for (int i = 0; i < bitmap.Width; i++)
+                {
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        int R = 1;                        
+                        while (bitArray2[i, j] == 0)
+                        {
+                            int i1, j1;
+                            i1 = i + R;
+                            for (j1 = j - R; j1 <= j + R; j1++)
+                            {
+                                   if (findRadius(i, j, i1, j1, R)) {
+                                        break;
+                                   }
+                            }
+
+                            j1 = j + R;
+                            for (i1 = i - R + 1; i1 < i + R; i1++)
+                            {
+                                if (findRadius(i, j, i1, j1, R))
+                                {
+                                    break;
+                                }
+                            }
+
+                            i1 = i - R;
+                            for (j1 = j - R; j1 <= j + R; j1++)
+                            {
+                                if (findRadius(i, j, i1, j1, R))
+                                {
+                                    break;
+                                }
+                            }
+
+                            j1 = j - R;
+                            for (i1 = i - R + 1; i1 < i + R; i1++)
+                            {
+                                if (findRadius(i, j, i1, j1, R))
+                                {
+                                    break;
+                                }
+                            }
+                            R++;
+                            
+                        }
+                        dataGridView2.Rows[j].Cells[i].Value = bitArray2[i, j];
+                        bitArray3 = convertBinToGray(bitArray2);
+                        bitmap2.SetPixel(i, j, Color.FromArgb(bitArray3[i, j], bitArray3[i, j], bitArray3[i, j]));
+                    }
+                }
+
+                pictureBox2.Image = bitmap2;
+
+            }
+            
+        }
+
+        private void count() { }
+    }
+}
---
+++ lab1_2010/MainForm.designer.cs
@@ -0,0 +1,135 @@
+﻿namespace lab1_2010
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(12, 438);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(96, 36);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Откыть";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(120, 428);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(257, 185);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(365, 410);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(383, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(409, 410);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(383, 428);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(257, 185);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(798, 12);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(365, 410);
+            this.dataGridView3.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 632);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.openFileButton);
+            this.Name = "MainForm";
+            this.Text = "Graphic Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+    }
+}
+
